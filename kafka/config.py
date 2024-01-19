9106811c4fbf01884aafc6ddac97e504bd7a233d
Noctibus <stanislas.debet@gmail.com>
2024-01-19 14:22:25 +0100

    Base KAFKA

---
+++ kafka/config.py
@@ -0,0 +1,9 @@
+USER="cytech",
+PASSWORD="cytech",
+HOST="localhost",
+PORT="5432",
+DATABASE="trackinggps_bdd"
+TABLE="location_history"
+
+KAFKA_TOPIC="trackingGPS"
+KAFKA_SERVER_ADDRESS="localhost:9092"
--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,9 +1,28 @@
 from kafka import KafkaConsumer
+import json
+import config
 
-topic = 'trackingGPS'
 
-consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest')
+# Read messages from Kafka.
+def read_messages():
+    # Get variables from config file
+    topic = config.KAFKA_TOPIC
+    bootstra_servers = config.KAFKA_SERVER_ADDRESS
 
-print('Starting the consumer')
-for message in consumer:
-    print(message)
+    # Create a Kafka consumer
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=bootstra_servers,
+        auto_offset_reset="earliest",
+        group_id="consumer-group-a"
+    )
+
+    # Read messages from Kafka
+    for message in consumer:
+        # convert to json
+        msg = json.loads(message.value)
+        #TODO import func from package, or define func above ; and execute it as following :
+        # func(msg) ; with msg = {"id":un id, "lat": une latitude, "long": une longitude}
+
+if __name__ == "__main__":
+    read_messages()
