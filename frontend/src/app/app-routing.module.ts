ac03d9fdefc746b0e492603716a4a9cba908fa84
CYTech Student <madrelleal@cy-tech.fr>
2024-01-29 22:42:23 +0100

    doc

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 import { TrackingComponent } from './component/tracking/tracking.component';
+import { DocComponent } from './component/doc/doc.component';
 
 const routes: Routes = [
   { path: '', component : HomeComponent},
   { path: 'home', component : HomeComponent}, 
   { path: 'tracking', component: TrackingComponent},
+  { path: 'doc', component: DocComponent},
   { path: '**', redirectTo: '' } 
 ];
 
